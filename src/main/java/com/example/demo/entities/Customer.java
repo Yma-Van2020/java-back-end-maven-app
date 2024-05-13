93e110b712aa37cc233100f8ef2286940d7958b5
Admin <yma14@wgu.edu>
2024-05-12 19:24:07 -0700

    Reworked on Part G.

--- src/main/java/com/example/demo/entities/Customer.java
+++ src/main/java/com/example/demo/entities/Customer.java
@@ -1,10 +1,11 @@
 package com.example.demo.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -18,39 +19,48 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Customer {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "customer_id")
     private Long id;
 
+    @NotBlank(message = "First name is required")
     @Column(name = "customer_first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "postal_code", nullable = false)
-    private String postal_code;
-
-    @Column(name = "phone", nullable = false)
-    private String phone;
-
-    @CreationTimestamp
-    @Column(name = "create_date")
-    private Date create_date;
-
-    @UpdateTimestamp
-    @Column(name = "last_update")
-    private Date last_update;
-
+    @NotBlank(message = "Last name is required")
     @Column(name = "customer_last_name", nullable = false)
     private String lastName;
 
+    @NotBlank(message = "Postal code is required")
+    @Size(min = 5, max = 10, message = "Postal code must be between 5 and 10 characters")
+    @Column(name = "postal_code", nullable = false)
+    private String postalCode;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "\\(\\d{3}\\)\\d{3}-\\d{4}", message = "Phone number must be in the format (555)555-5555")
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @NotBlank(message = "Address is required")
     @Column(name = "address", nullable = false)
     private String address;
+
+    @CreationTimestamp
+    @Column(name = "create_date")
+    private Date createDate;
+
+    @UpdateTimestamp
+    @Column(name = "last_update", nullable = false)
+    private Date lastUpdate;
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Cart> carts;
 
+    @NotNull(message = "Division is required")
     @ManyToOne
-    @JoinColumn(name = "division_id")
+    @JoinColumn(name = "division_id", nullable = false)
     private Division division;
 
     public void add(Cart cart) {
