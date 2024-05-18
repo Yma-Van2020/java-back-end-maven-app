ad5e56e13af938e3564fa91c181c409bb24f7773
Katelyn Ma <Katelyn.Ma@modo.coop>
2024-05-18 14:32:07 -0700

    removed unneeded parts.

--- src/main/java/com/example/demo/services/CheckoutServiceImpl.java
+++ src/main/java/com/example/demo/services/CheckoutServiceImpl.java
@@ -2,13 +2,11 @@ package com.example.demo.services;
 
 import com.example.demo.dao.CartItemRepository;
 import com.example.demo.dao.CartRepository;
-import com.example.demo.dao.CustomerRepository;
 import com.example.demo.entities.Cart;
 import com.example.demo.entities.CartItem;
-import com.example.demo.entities.Customer;
+//import com.example.demo.entities.Customer;
 import com.example.demo.entities.StatusType;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -31,7 +29,7 @@ public class CheckoutServiceImpl implements CheckoutService {
     public PurchaseResponse placeOrder(Purchase purchase) {
 
         Cart cart = purchase.getCart();
-        Customer customer = purchase.getCustomer();
+//        Customer customer = purchase.getCustomer();
         Set<CartItem> cartItems = purchase.getCartItems();
 
         if (cart == null || cartItems == null || cartItems.isEmpty()) {
